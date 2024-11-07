0769882d5aee84eb16fe3ce44526ea103ff73ee7
Tharayil Joseph <joseph.tharayil@epfl.ch>
2024-11-07 17:17:41 +0100

    Updates

--- EditSpikeFile.sh
+++ EditSpikeFile.sh
@@ -37,25 +37,20 @@ mkdir NoL4Inh
 mkdir NoL5Inh
 mkdir NoL6Inh
 
-mkdir NoL5MC
 mkdir NoL5LBC
 mkdir NoL5NBC
+mkdir NoL5LBC_rewired
+mkdir NoL5NBC_rewired
 
-mkdir NoL5DBC_rewired
-mkdir NoL5DBC
-
-mkdir NoL5BP
-mkdir NoL5BP_rewired
-
-srun -n 10 python editSpikeFile.py 'original/1a8bf077-4f6c-4bcb-b257-ca1f3d2388cd' 'Layer23Excitatory' 'NoL23Exc/NoL23Exc' 'True' &
-srun -n 10 python editSpikeFile.py 'original/1a8bf077-4f6c-4bcb-b257-ca1f3d2388cd' 'Layer4Excitatory' 'NoL4Exc/NoL4Exc' 'True' &
-srun -n 10 python editSpikeFile.py 'original/1a8bf077-4f6c-4bcb-b257-ca1f3d2388cd' 'Layer5Excitatory' 'NoL5Exc/NoL5Exc' 'True' &
-srun -n 10 python editSpikeFile.py 'original/1a8bf077-4f6c-4bcb-b257-ca1f3d2388cd' 'Layer6Excitatory' 'NoL6Exc/NoL6Exc' 'True' &
-srun -n 10 python editSpikeFile.py 'original/1a8bf077-4f6c-4bcb-b257-ca1f3d2388cd' 'Layer1Inhibitory' 'NoL1Inh/NoL1Inh' 'True' &
-srun -n 10 python editSpikeFile.py 'original/1a8bf077-4f6c-4bcb-b257-ca1f3d2388cd' 'Layer23Inhibitory' 'NoL23Inh/NoL23Inh' 'True' &
-srun -n 10 python editSpikeFile.py 'original/1a8bf077-4f6c-4bcb-b257-ca1f3d2388cd' 'Layer4Inhibitory' 'NoL4Inh/NoL4Inh' 'True' &
-srun -n 10 python editSpikeFile.py 'original/1a8bf077-4f6c-4bcb-b257-ca1f3d2388cd' 'Layer5Inhibitory' 'NoL5Inh/NoL5Inh' 'True' &
-srun -n 10 python editSpikeFile.py 'original/1a8bf077-4f6c-4bcb-b257-ca1f3d2388cd' 'Layer6Inhibitory' 'NoL6Inh/NoL6Inh' 'True' &
+srun -n 10 python editSpikeFile.py 'original/fixed/a9f782a3-1f22-4384-a122-430bc6b2323c' 'Layer23Excitatory' 'NoL23Exc/NoL23Exc' 'True' &
+srun -n 10 python editSpikeFile.py 'original/fixed/a9f782a3-1f22-4384-a122-430bc6b2323c' 'Layer4Excitatory' 'NoL4Exc/NoL4Exc' 'True' &
+srun -n 10 python editSpikeFile.py 'original/fixed/a9f782a3-1f22-4384-a122-430bc6b2323c' 'Layer5Excitatory' 'NoL5Exc/NoL5Exc' 'True' &
+srun -n 10 python editSpikeFile.py 'original/fixed/a9f782a3-1f22-4384-a122-430bc6b2323c' 'Layer6Excitatory' 'NoL6Exc/NoL6Exc' 'True' &
+srun -n 10 python editSpikeFile.py 'original/fixed/a9f782a3-1f22-4384-a122-430bc6b2323c' 'Layer1Inhibitory' 'NoL1Inh/NoL1Inh' 'True' &
+srun -n 10 python editSpikeFile.py 'original/fixed/a9f782a3-1f22-4384-a122-430bc6b2323c' 'Layer23Inhibitory' 'NoL23Inh/NoL23Inh' 'True' &
+srun -n 10 python editSpikeFile.py 'original/fixed/a9f782a3-1f22-4384-a122-430bc6b2323c' 'Layer4Inhibitory' 'NoL4Inh/NoL4Inh' 'True' &
+srun -n 10 python editSpikeFile.py 'original/fixed/a9f782a3-1f22-4384-a122-430bc6b2323c' 'Layer5Inhibitory' 'NoL5Inh/NoL5Inh' 'True' &
+srun -n 10 python editSpikeFile.py 'original/fixed/a9f782a3-1f22-4384-a122-430bc6b2323c' 'Layer6Inhibitory' 'NoL6Inh/NoL6Inh' 'True' &
 
 srun -n 10 python editSpikeFile.py 'rewired/97d6aa07-db02-48c6-91c2-b3023ce5bdd0' 'Layer23Excitatory' 'NoL23Exc_rewired/NoL23Exc_rewired' 'True' &
 srun -n 10 python editSpikeFile.py 'rewired/97d6aa07-db02-48c6-91c2-b3023ce5bdd0' 'Layer4Excitatory' 'NoL4Exc_rewired/NoL4Exc_rewired' 'True' &
@@ -70,7 +65,7 @@ srun -n 10 python editSpikeFile.py 'rewired/97d6aa07-db02-48c6-91c2-b3023ce5bdd0
 srun -n 10 python editSpikeFile.py 'rewired/97d6aa07-db02-48c6-91c2-b3023ce5bdd0' 'L5_LBC' 'NoL5LBC_rewired/NoL5LBC_rewired' 'True' &
 srun -n 10 python editSpikeFile.py 'rewired/97d6aa07-db02-48c6-91c2-b3023ce5bdd0' 'L5_NBC' 'NoL5NBC_rewired/NoL5NBC_rewired' 'True' &
 
-srun -n 10 python editSpikeFile.py 'original/1a8bf077-4f6c-4bcb-b257-ca1f3d2388cd' 'L5_LBC' 'NoL5LBC/NoL5LBC' 'True' &
-srun -n 10 python editSpikeFile.py 'original/1a8bf077-4f6c-4bcb-b257-ca1f3d2388cd' 'L5_BTC' 'NoL5BTC/NoL5BTC' 'True' &
+srun -n 10 python editSpikeFile.py 'original/fixed/a9f782a3-1f22-4384-a122-430bc6b2323c' 'L5_LBC' 'NoL5LBC/NoL5LBC' 'True' &
+srun -n 10 python editSpikeFile.py 'original/fixed/a9f782a3-1f22-4384-a122-430bc6b2323c' 'L5_NBC' 'NoL5NBC/NoL5NBC' 'True' &
 
 wait
